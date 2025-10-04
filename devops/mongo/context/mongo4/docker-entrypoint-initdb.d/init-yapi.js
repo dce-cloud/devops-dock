908209eff566470a104e2d3a39b150b619c9c906
xulei131401 <xulei13140107@126.com>
2025-10-04 23:39:16 +0800

    脚本自动push

--- devops/mongo/context/mongo4/docker-entrypoint-initdb.d/init-yapi.js
+++ devops/mongo/context/mongo4/docker-entrypoint-initdb.d/init-yapi.js
@@ -1,34 +1,45 @@
+const root_user_config = {
+  user: 'root',
+  password: 'SZsBH3@ueNUPexFI',
+}
+
+const yapi_user_config = {
+  user: 'yapi',
+  password: '123456',
+  db: 'yapi'
+}
+
 db = db.getSiblingDB('admin');
 
-const rootAuth = db.auth('root', 'SZsBH3@ueNUPexFI');
+const rootAuth = db.auth(root_user_config.user, root_user_config.password);
 if (!rootAuth) {
-  throw new Error('root 用户鉴权失败，请检查用户名和密码');
+  throw new Error(`${root_user_config.user} 用户鉴权失败，请检查用户名和密码`);
 }
-print('root 用户鉴权成功');
+print(`${root_user_config.user} 用户鉴权成功`);
 
-const yapiDb = db.getSiblingDB('yapi');
+const yapiDb = db.getSiblingDB(yapi_user_config.db);
 
 try {
   yapiDb.createUser({
-    user: "yapi",
-    pwd: "123456",
-    roles: [{ role: "readWrite", db: "yapi" }]
+    user: yapi_user_config.user,
+    pwd: yapi_user_config.password,
+    roles: [{ role: "readWrite", db: yapi_user_config.db}]
   });
-  print('my_yapi 用户创建成功');
+  print('yapi 用户创建成功');
 } catch (err) {
   if (err.code === 11000) { // 用户名已存在的错误码
-    print('yapi 用户已存在，跳过创建');
+    print(`${yapi_user_config.user} 用户已存在，跳过创建`);
   } else {
     throw err;
   }
 }
 
-const userAuth = yapiDb.auth('yapi', '123456');
+const userAuth = yapiDb.auth(yapi_user_config.user, yapi_user_config.password);
 if (userAuth) {
-  print('my_yapi 用户鉴权成功');
+  print(`${yapi_user_config.user} 用户鉴权成功`);
 } else {
-  throw new Error('my_yapi 用户鉴权失败');
+  throw new Error(`${yapi_user_config.user} 用户鉴权失败`);
 }
 
 print('\n可通过以下命令登录：');
-print(`./mongo 127.0.0.1:27017/yapi -u "my_yapi" -p "123456"`);
+print(`./mongo 127.0.0.1:27017/${yapi_user_config.db} -u "${yapi_user_config.user}" -p "${yapi_user_config.password}"`);
